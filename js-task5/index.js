888aec52bb2ad3c75ef928cfc69e010dcf1bd353
Lizaveta Kaliaha <liza-98k@mail.ru>
2018-07-20 14:22:36 +0300

    add task6

--- js-task5/index.js
+++ js-task5/index.js
@@ -1,10 +1,29 @@
-//1 откуда у DoSomething videos?? выполнение обоих промисов
+//1  выполнение обоих промисов (рабочий пример)
 (function task5_a() {
     'use strict';
 
+    function loadVideosAsync() {
+      return  new Promise(function (resolve) {
+            setTimeout(() => {
+                resolve("video");
+            }, 4000)
+        })
+    }
+    function loadMetaAsync() {
+        return  new Promise(function (resolve) {
+            setTimeout(() => {
+                resolve("meta");
+            }, 2000)
+        })
+    }
+    function DoSomething(videos,meta) {
+       console.log(videos + meta);
+
+    };
+
     Promise.all([loadVideosAsync(), loadMetaAsync()])
-        .then(function (videos, meta) {
-            DoSomething(videos, meta);
+        .then(function (result) {
+            DoSomething(result[0],result[1]);
         });
 })()
 
@@ -13,13 +32,26 @@
 (function task5_b() {
     'use strict';
 
-    async function anAsyncCall() {
+    function somethingComplicated() {
+      return  new Promise(()=> {
+            setTimeout(() => {
+            }, 1000)
+        })
+     }
+    function doSomethingAsync() {
+        return  new Promise(()=>{
+            setTimeout(() => {
+            }, 2000)
+        })
+    }
+    (async function anAsyncCall() {
         var promise = await somethingComplicated(await doSomethingAsync());
         return promise;
-    };
+    })();
+
 })()
 
-//3 проверка если все удалились
+// //3 проверка если все удалились
 (function task5_c() {
     'use strict';
 
