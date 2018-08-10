74e9a86b44f05977964b5f692a262b34d23efbf1
Lizaveta Kaliaha <liza-98k@mail.ru>
2018-08-10 12:47:57 +0300

    init task3

---
+++ 4_ASP.NET_CORE/task3/task3WebAPI/task3WebAPI/Controllers/FilmsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task3WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
