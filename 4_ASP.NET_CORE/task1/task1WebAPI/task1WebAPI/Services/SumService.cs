971f9bc7ecf5e5a92cf7935a1494e7916e9a6300
Lizaveta Kaliaha <liza-98k@mail.ru>
2018-08-15 11:29:22 +0300

    task 1 add SumService

---
+++ 4_ASP.NET_CORE/task1/task1WebAPI/task1WebAPI/Services/SumService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task1WebAPI.Services
+{
+    public class SumService : ISumService
+    {
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- 4_ASP.NET_CORE/task1/task1WebAPI/task1WebAPI/Startup.cs
+++ 4_ASP.NET_CORE/task1/task1WebAPI/task1WebAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using task1WebAPI.Services;
 
 namespace task1WebAPI
 {
@@ -25,6 +26,7 @@ namespace task1WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ISumService, SumService>();
             services.AddMvc().AddXmlDataContractSerializerFormatters();
         }
 
