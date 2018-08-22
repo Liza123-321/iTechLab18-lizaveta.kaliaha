02d835d3b185efac5b10aaab4e3e1ced69a62396
Lizaveta Kaliaha <liza-98k@mail.ru>
2018-08-22 16:49:59 +0300

    add register validation

--- 4_ASP.NET_CORE/task4/client_task4/client-task4/src/containers/RegisterContainer.js
+++ 4_ASP.NET_CORE/task4/client_task4/client-task4/src/containers/RegisterContainer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Register from '../views/Register/index';
 import axios from 'axios';
-import { validateForm, validateField } from '../validation';
+import { validateFieldRegister, validateFormRegister } from '../validation';
 
 class RegisterContainer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleUserInput = this.handleUserInput.bind(this);
-		this.validateField = validateField.bind(this);
-		this.validateForm = validateForm.bind(this);
+		this.validateFieldRegister = validateFieldRegister.bind(this);
+		this.validateFormRegister = validateFormRegister.bind(this);
 		this.registerClick = this.registerClick.bind(this);
 		this.state = {
 			email: '',
@@ -30,7 +30,7 @@ class RegisterContainer extends React.Component {
 		const name = e.target.id;
 		const value = e.target.value;
 		this.setState({ [name]: value }, () => {
-			this.validateField(name, value);
+			this.validateFieldRegister(name, value);
 		});
 	};
 
@@ -42,10 +42,25 @@ class RegisterContainer extends React.Component {
 				password: self.state.password,
 			})
 			.then(function(res) {
-				console.log(res);
 				if (res.data) {
 					alert('Success register');
 				}
+			})
+			.then(function() {
+				axios
+					.post(`https://localhost:5001/api/user/login`, {
+						email: self.state.email,
+						password: self.state.password,
+					})
+					.then(function(res) {
+						if (res.data) {
+							alert('Success autorize');
+							sessionStorage.setItem('jwt_token', res.data.access_token);
+						} else alert('Cant find user with this email and password');
+					});
+			})
+			.catch(function(res) {
+				alert('You cant use this email!');
 			});
 	}
 
--- 4_ASP.NET_CORE/task4/client_task4/client-task4/src/validation/index.js
+++ 4_ASP.NET_CORE/task4/client_task4/client-task4/src/validation/index.js
@@ -25,9 +25,51 @@ export function validateField(fieldName, value) {
 		this.validateForm
 	);
 }
+export function validateFieldRegister(fieldName, value) {
+	let fieldValidationErrors = this.state.formErrors;
+	let emailValid = this.state.emailValid;
+	let confirmValid = this.state.confirmValid;
+	let passwordValid = this.state.passwordValid;
+	let currentPassword = this.state.password;
+	switch (fieldName) {
+		case 'email':
+			emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+			fieldValidationErrors.email = emailValid ? '' : ' Invalid email';
+			break;
+		case 'password':
+			passwordValid = value.length >= 5;
+			fieldValidationErrors.password = passwordValid
+				? ''
+				: 'Password is to short';
+			break;
+		case 'confirm':
+			confirmValid = currentPassword === value;
+			fieldValidationErrors.confirm = confirmValid ? '' : 'Confirm != password';
+			break;
+		default:
+			break;
+	}
+	this.setState(
+		{
+			formErrors: fieldValidationErrors,
+			confirmValid: confirmValid,
+			emailValid: emailValid,
+			passwordValid: passwordValid,
+		},
+		this.validateFormRegister
+	);
+}
 
 export function validateForm() {
 	this.setState({
 		formValid: this.state.emailValid && this.state.passwordValid,
+	});
+}
+export function validateFormRegister() {
+	this.setState({
+		formValid:
+			this.state.emailValid &&
+			this.state.passwordValid &&
+			this.state.confirmValid,
 	});
 }
